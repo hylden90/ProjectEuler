2164b76b510df0a4a267f92ef65f5a4feba5d043
Dihego <dfbautistat@bitbucket.org>
2018-12-24 02:45:38 -0500

    Maven project scaffolding: -Generation of basic archetype. -Added lines onto .gitignore regarding vscode.

---
+++ projectEuler/src/main/java/com/hylden90/projectEuler/App.java
@@ -0,0 +1,13 @@
+package com.hylden90.projectEuler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
