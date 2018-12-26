a6bf810edfdbff192447bcfa7395154efb4353fc
Dihego <dfbautistat@bitbucket.org>
2018-12-25 23:51:57 -0500

    Code to get second problem answer.

--- projectEuler/src/main/java/com/hylden90/projectEuler/App.java
+++ projectEuler/src/main/java/com/hylden90/projectEuler/App.java
@@ -1,7 +1,7 @@
 package com.hylden90.projectEuler;
 
 import static com.hylden90.projectEuler.problem1.Problem1.sumMultiplesOf3And5Below;
-
+import static com.hylden90.projectEuler.problem2.Problem2.sumEvenFibonacciNumbersLowerThan;
 /**
  * Hello world!
  *
@@ -11,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         int problem1Answer = sumMultiplesOf3And5Below(1000); //233168
+        int problem2Answer = sumEvenFibonacciNumbersLowerThan(4000000);
     }
 }
