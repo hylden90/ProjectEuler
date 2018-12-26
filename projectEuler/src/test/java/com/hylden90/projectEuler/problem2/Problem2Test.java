c553f051fbf6c0780cc9c0c1f0a05bd144b79777
Dihego <dfbautistat@bitbucket.org>
2018-12-25 23:37:54 -0500

    First solution trial for second problem. Tests pass so far. Will it be enough for a large limit?

--- projectEuler/src/test/java/com/hylden90/projectEuler/problem2/Problem2Test.java
+++ projectEuler/src/test/java/com/hylden90/projectEuler/problem2/Problem2Test.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import static com.hylden90.projectEuler.problem2.Problem2.sumEvenFibonacciNumbersLowerThan;
+
 public class Problem2Test
 {
     @Test
