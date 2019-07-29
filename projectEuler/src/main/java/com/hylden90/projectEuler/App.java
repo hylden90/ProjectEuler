4dc8ba63fbd26b90f17dbe4c9360e64545c11f29
Dihego <dfbautistat@bitbucket.org>
2019-07-29 16:05:44 -0500

    Code to get solution for problem3.

--- projectEuler/src/main/java/com/hylden90/projectEuler/App.java
+++ projectEuler/src/main/java/com/hylden90/projectEuler/App.java
@@ -2,6 +2,7 @@ package com.hylden90.projectEuler;
 
 import static com.hylden90.projectEuler.problem1.Problem1.sumMultiplesOf3And5Below;
 import static com.hylden90.projectEuler.problem2.Problem2.sumEvenFibonacciNumbersLowerThan;
+import static com.hylden90.projectEuler.problem3.Problem3.getLargestPrimeFactor;
 /**
  * Hello world!
  *
@@ -12,5 +13,6 @@ public class App
     {
         int problem1Answer = sumMultiplesOf3And5Below(1000); //233168
         int problem2Answer = sumEvenFibonacciNumbersLowerThan(4000000);
+        int problem3Answer = getLargestPrimeFactor(600851475143L);
     }
 }
